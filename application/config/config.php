0c666de46901b6754c811b9af2a46a6c3952ec1c
sct <sctsnipe@gmail.com>
2013-01-08 18:09:09 -0500

    Readded .htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://www.sctgaming.com/Technic/API/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
