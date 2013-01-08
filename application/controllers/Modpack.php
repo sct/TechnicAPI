06bb7c807b646723f48b845d8e2b6c09f39caccd
sct <sctsnipe@gmail.com>
2013-01-08 18:03:35 -0500

    Modpack build details

--- application/controllers/Modpack.php
+++ application/controllers/Modpack.php
@@ -20,6 +20,7 @@ class Modpack extends REST_Controller
 		} else { $request = NULL; }
 
 		$yaml = file_get_contents('http://mirror.technicpack.net/Technic/'.$this->uri->segment(2).'/modpack.yml');
+		$modpackData = Spyc::YAMLLoad($yaml);
 				
 		switch ($request) 
 		{
@@ -27,9 +28,16 @@ class Modpack extends REST_Controller
 				$md5 = array("md5" => md5($yaml));
 				$this->response($md5);
 				break;
+			case "build":
+				if ($this->uri->segment(4) == NULL OR !array_key_exists($this->uri->segment(4),$modpackData['builds'])) {
+					$this->response(array('error' => 'Build not found'),404);
+				} else {
+					$buildRequest = $this->uri->segment(4);
+					$versionData = $modpackData['builds'][$buildRequest];
+					$this->response($versionData);
+				}
+				break;
 			default:
-				$modpackData = Spyc::YAMLLoad($yaml);
-
 				// Generate build list
 				$modpackBuilds = array();
 				foreach ($modpackData['builds'] as $build => $value)
