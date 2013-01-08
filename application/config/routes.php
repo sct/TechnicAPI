613bbdccc13dec771a7880a1ddc7051375a17dd4
sct <sctsnipe@gmail.com>
2013-01-08 17:53:03 -0500

    Basic Modpack API implemented
    - Modpack listing
    - Modpack details
    - Modpack YML MD5

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,7 @@
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
+$route['modpack/(:any)'] = 'modpack/details/$1';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
--- application/controllers/Modpack.php
+++ application/controllers/Modpack.php
@@ -1,13 +1,49 @@
 <?php
 
 require_once(APPPATH.'libraries/REST_Controller.php'); 
+require_once(APPPATH.'third_party/Spyc.php'); 
 
 class Modpack extends REST_Controller
 {
 	
 	public function index_get()
 	{
+		$yaml = file_get_contents('http://mirror.technicpack.net/Technic/modpacks.yml');
+		$modpacks = Spyc::YAMLLoad($yaml);
+		$this->response($modpacks);
+	}
 
+	public function details_get()
+	{
+		if ($this->uri->segment(3) != NULL) {
+			$request = $this->uri->segment(3);
+		} else { $request = NULL; }
+
+		$yaml = file_get_contents('http://mirror.technicpack.net/Technic/'.$this->uri->segment(2).'/modpack.yml');
+				
+		switch ($request) 
+		{
+			case "MD5":
+				$md5 = array("md5" => md5($yaml));
+				$this->response($md5);
+				break;
+			default:
+				$modpackData = Spyc::YAMLLoad($yaml);
+
+				// Generate build list
+				$modpackBuilds = array();
+				foreach ($modpackData['builds'] as $build => $value)
+				{
+					array_push($modpackBuilds,$build);
+				}
+
+				$modpack = array(
+								"recommended" => $modpackData['recommended'],
+								"latest" => $modpackData['latest'],
+								"builds" => $modpackBuilds
+								);
+				$this->response($modpack);
+		}
 	}
 
 }
